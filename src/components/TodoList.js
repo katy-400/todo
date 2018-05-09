f9da9ff1b9ad92390d6cbb877e0c05da9f6dfa52
katy-400 <katy.nyby@gmail.com>
2018-05-09 11:35:22 -0400

    Created import statements

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,3 +13,16 @@ const TodoList = ({ todos, toggleTodo }) => (
     )}
   </ul>
 )
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired
+}
+
+export default TodoList
