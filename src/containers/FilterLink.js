86dfd29a506cc7655cb9a026370988cbd8fba308
katy-400 <katy.nyby@gmail.com>
2018-05-09 11:57:36 -0400

    added import statements from redux

---
+++ src/containers/FilterLink.js
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../actions'
+import Link from '../components/Link'
+
+const mapStateToProps = (state, ownProps) => ({
+  active: ownProps.filter === state.visibilityFilter
+})
