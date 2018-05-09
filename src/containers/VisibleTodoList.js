588d7dc8836ac0445644903f86ce42ffe3ef34aa
katy-400 <katy.nyby@gmail.com>
2018-05-09 11:56:45 -0400

    export statement connecting it to maps

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -19,3 +19,12 @@ const getVisibleTodos = (todos, filter) => {
 const mapStateToProps = state => ({
   todos: getVisibleTodos(state.todos, state.visibilityFilter)
 })
+
+const mapDispatchToProps = dispatch => ({
+  toggleTodo: id => dispatch(toggleTodo(id))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
