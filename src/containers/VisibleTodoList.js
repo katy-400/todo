2e030dc304f777ff4ceb72872f461c263aeb39e7
katy-400 <katy.nyby@gmail.com>
2018-05-09 11:52:38 -0400

    added statements for visibility filters

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -2,3 +2,16 @@ import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
 import { VisibilityFilters } from '../actions'
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(t => t.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed)
+    default:
+      throw new Error('Unknown filter: ' + filter)
+  }
+}
