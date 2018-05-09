aea28822be77c7ca0332ab5efb1edcf491464258
katy-400 <katy.nyby@gmail.com>
2018-05-09 11:30:26 -0400

    added todo list files that will make the list
    import statements, onclick events, constructor todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,3 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-const Todo = ({ onClick, completed, text }) => (
-	<li
-		onClick={onClick}
-		style={ {
-		  textDecoration: completed ? 'line-through' : 'none'
-		}}
-	>
-    {text}
-  </li>
-)
+import Todo from './Todo'
